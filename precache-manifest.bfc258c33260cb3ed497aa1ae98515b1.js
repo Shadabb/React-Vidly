91cd22e4f55fac07ce7010d0a67d38b7be1d0b50
Shadab Akhtar <LA-SUP-SAKHTAR@wwcorp.ad.com>
2019-02-25 18:11:39 +0800

    Updates

---
+++ precache-manifest.bfc258c33260cb3ed497aa1ae98515b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b9a33f20ac419381137",
+    "url": "/Shadabb/React-Vidly/static/css/main.c4f94c45.chunk.css"
+  },
+  {
+    "revision": "0b9a33f20ac419381137",
+    "url": "/Shadabb/React-Vidly/static/js/main.0b9a33f2.chunk.js"
+  },
+  {
+    "revision": "f429c58089d1b1dc865b",
+    "url": "/Shadabb/React-Vidly/static/css/1.f2b9364a.chunk.css"
+  },
+  {
+    "revision": "f429c58089d1b1dc865b",
+    "url": "/Shadabb/React-Vidly/static/js/1.f429c580.chunk.js"
+  },
+  {
+    "revision": "5b42b94e9b3d2970854b",
+    "url": "/Shadabb/React-Vidly/static/js/runtime~main.5b42b94e.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Shadabb/React-Vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Shadabb/React-Vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Shadabb/React-Vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Shadabb/React-Vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Shadabb/React-Vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a2711ba1b8f5fd519a3f200b19aa614f",
+    "url": "/Shadabb/React-Vidly/index.html"
+  }
+];
--- src/components/not-found.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-const Notfound = () => {
-  return <h1>Not Found</h1>;
-};
-
-export default Notfound;
